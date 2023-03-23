5a3bbad6b9ce74f61d36e9930aa8e2068d0f8084
Liam Cammpbell <liam22campbell@gmail.com>
2023-03-23 15:08:44 -0700

    Add details to race page

--- src/CharacterCreationModule/AddRace.jsx
+++ src/CharacterCreationModule/AddRace.jsx
@@ -5,24 +5,79 @@ import { FormContext } from './NewCharacter';
 import * as yup from 'yup';
 import { Formik } from "formik";
 
+
+
+ // Add ability choice options
+
 const getRaceInfo = (data, race) => {
   return Object.values(data.default).filter((details) => details.name === race);
 };
+
+const getAbilityBonuses = (data) => {
+  let abilityArray = [];
+  Object.values(data).forEach(ability => abilityArray.push(ability.ability_score?.index));
+  
+  return abilityArray;
+} 
+
+const getIncreaseAmount = (data) => {
+  let abilityBonusArray = [];
+  Object.values(data).forEach(ability => abilityBonusArray.push(ability.bonus));
+  return abilityBonusArray;
+  
+}
+
+
+const getAvgAge = (ageDescription) => {
+  
+  // let regExp = /([Aa]dult)/g;
+  let numRegExp = /(\d+)/g;
+  let array = [];
+  let ageDescriptionArray = array.concat(ageDescription.match(numRegExp));
+  if(ageDescriptionArray.length === 0){
+    return 30;
+  }else if(ageDescriptionArray.length >= 3){
+    return ageDescriptionArray[2];
+  }else if (ageDescriptionArray.length < 3){
+    return ageDescriptionArray[0];
+  }else{
+    return ageDescriptionArray[1];
+  }
+  
+}
+
+const combineAbilityBonuses = (abilityName, bonusAmount) => {
+  let bonusObject = {};
+  if(abilityName !== null){
+    for(let i = 0; i < abilityName.length; i ++){
+      let key = abilityName[i];
+      bonusObject[key] = bonusAmount[i];
+    }
+  }
+  return bonusObject;
+}
+
+
+
 const AddRaceDetails = () => {
   const { nextStep, prevStep, currentStep, formData, setFormData, values } = useContext(FormContext);
-  console.log(formData, 'form data');
-  console.log(getRaceInfo(data, "Dragonborn")[0]);
-  const raceDetails = getRaceInfo(data, "Dragonborn")[0];
-  const abilityBonuses = raceDetails.ability_bonuses;
-  console.log(abilityBonuses);
-  
+ 
+  console.log(getRaceInfo(data, "Human")[0]);
+  const raceDetails = getRaceInfo(data, "Dwarf")[0];
+  const characterAbilityBonuses = getAbilityBonuses(raceDetails.ability_bonuses);
+  const characterBonusAmounts = getIncreaseAmount(raceDetails.ability_bonuses);
+  let averageRaceAge = getAvgAge(raceDetails.age);
+  let abilityBonuses = combineAbilityBonuses(characterAbilityBonuses, characterBonusAmounts);
   const characterSchema = yup.object().shape({
     size: yup.string().required(),
-    speed: yup.number().required()
+    speed: yup.number().required(),
+    abilityBonus: yup.object().required(),
+    age: yup.number().required(),
   })
   const handleFormSubmit= (values) => {
     const data = { ...formData, ...values};
     setFormData(data);
+    console.log(formData);
     nextStep();
   }
   return (
@@ -31,6 +86,11 @@ const AddRaceDetails = () => {
         initialValues={{
           size: raceDetails.size,
           speed: raceDetails.speed,
+          abilityBonusArray: characterAbilityBonuses, 
+          abilityBonusAmount: characterBonusAmounts,
+          abilityBonus: abilityBonuses,
+          age: getAvgAge(raceDetails.age),
+          
         }}
           onSubmit={handleFormSubmit}
           validationSchema={characterSchema}
@@ -51,13 +111,13 @@ const AddRaceDetails = () => {
               type="text"
               label="Speed"
               onBlur={handleBlur}
-              // onChange={handleFormData("firstName")}
+              onChange={handleChange}
               placeholder={raceDetails.speed}
               value={values.speed}
               name="speed"
               errors={!!touched.speed && !!errors.speed}
               helperText={touched.speed && errors.speed}
-              sx={{ gridColumn: "span 2" }}
+              sx={{ gridColumn: "span 1" }}
             />
             <TextField
               fullWidth
@@ -65,14 +125,28 @@ const AddRaceDetails = () => {
               type="text"
               label="Size"
               onBlur={handleBlur}
-              // onChange={handleFormData("firstName")}
+              onChange={handleChange}
               placeholder={raceDetails.size}
               value={values.size}
               name="speed"
               errors={!!touched.size && !!errors.size}
               helperText={touched.size && errors.size}
-              sx={{ gridColumn: "span 2" }}
+              sx={{ gridColumn: "span 1" }}
             /> 
+             <TextField
+              fullWidth
+              variant="filled"
+              type="text"
+              label="Age"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              placeholder={averageRaceAge}
+              value={values.age}
+              name="age"
+              errors={!!touched.age && !!errors.age}
+              helperText={touched.speed && errors.age}
+              sx={{ gridColumn: "span 1" }}
+            />
             <Button type="submit" variant="contained">
                 Continue
             </Button>  
@@ -80,12 +154,67 @@ const AddRaceDetails = () => {
         )}
       </Formik>
       
-      <Box display="grid" justifyContent="start">
+      <Box display="grid" justifyContent="start" m='20px'>
         <Typography>Stats:</Typography>
-        <Box>
-          <Box>
-            <Typography>Speed: {raceDetails.speed} feet</Typography>
-            <Typography>Size: {raceDetails.size}</Typography>
+        <Box p='8px'>
+          <Box sx={{
+            display: 'grid',
+            gridTemplateRows: '1fr 1fr 1fr 1fr',
+          }}>
+            <Box sx={{
+              display: 'grid',
+              gridTemplateColumns: '1fr 1fr 2fr 2fr',
+              margin: '8px'
+            }}>
+              <Box>
+                <Typography m='3px'>Speed: {raceDetails.speed} feet</Typography>
+                <Typography m='3px'>Size: {raceDetails.size}</Typography> 
+              </Box>
+              <Box>
+                <Typography>Languages:</Typography>
+                {raceDetails.languages.map((lang, index) => (
+                  <Typography key={index}>
+                    {lang.name}
+                  </Typography>
+                ))}
+              </Box>
+              <Box>
+                <Typography>Traits: </Typography>
+                {raceDetails.traits.map((trait, index) => (
+                  <Typography key={index}>
+                    {trait.name}
+                  </Typography>
+                ))}
+              </Box>
+              <Box sx={{display: 'grid', justifyContent: 'center'}}>
+                <Typography>Ability Bonuses</Typography>
+                <Box sx={{display: 'grid', gridTemplateColumns: '.3fr .3fr'}}>
+                  <Box>{characterAbilityBonuses.map((ability, index) => (
+                    <Typography key={index}>{ability}</Typography>
+                  ))}</Box>
+                  <Box>{characterBonusAmounts.map((bonus, index) => (
+                    <Typography key={index}> + {bonus}</Typography>
+                  ))}</Box>
+                </Box>
+              </Box>
+            </Box>
+            <Box sx={{
+              display: 'grid',
+            }}>
+              
+              <Typography>Age: {raceDetails.age}</Typography>
+            </Box>
+            <Box sx={{
+              display: 'grid',
+            }}>
+              <Typography>{raceDetails.size_description}</Typography>
+            </Box>
+            <Box sx={{
+              display: 'grid',
+            }}>
+              
+              <Typography>{raceDetails.language_desc}</Typography>
+            </Box>
           </Box>
         </Box>
       </Box>
