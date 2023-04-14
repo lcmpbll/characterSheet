946ecd4f1c8bb8aeba1c3a1963fbc3c944e181f6
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-14 15:51:10 -0700

    Add format to add class details

---
+++ src/CharacterCreation/Forms/AddClass.jsx
@@ -0,0 +1,16 @@
+import React, {useState, useContext} from 'react';
+import Loading from '../../Components/loading';
+import {Box, Typography } from '@mui/material';
+// import InputField from '../../FormFields';
+import { FormContext } from '../CreateNewCharacter';
+
+
+export const AddClassDetails = ({props}) => {
+  console.log(props);
+  return (
+    <Box>
+      <h1>Class Details</h1>
+    </Box>
+    
+  )
+}
