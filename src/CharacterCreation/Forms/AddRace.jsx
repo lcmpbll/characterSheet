92bd7f04332cf3876b2cbe8265524da1c8214912
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-14 09:26:59 -0700

    Remove form api fetch, use HOC

--- src/CharacterCreation/Forms/AddRace.jsx
+++ src/CharacterCreation/Forms/AddRace.jsx
@@ -1,26 +1,15 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { FormContext } from '../CreateNewCharacter';
-// import * as data from "../../data/races.json";
+import React, {useState, useContext } from 'react';
+import Loading from '../../Components/loading';
 import { Box, Typography, TextField, Grid } from "@mui/material";
 import InputField from '../../FormFields/InputField';
-
-
-// const userAction = async () => {
-//   const response = await fetch('https://www.dnd5eapi.co/api/races');
-//   const data = await response.json();
-//   console.log(data);
-//   return data;//extract JSON from the http response
-//   // do something with myJson
-// }
+import { FormContext } from '../CreateNewCharacter';
 
  // Add ability choice options
  // Add proficiencies
  // add clickable for more information to traits, and languages
  // add slider for age selection?
 
-export const getRaceInfo = (data, race) => {
-  return Object.values(data.default).filter((details) => details.name === race);
-};
+
 
 const getAbilityBonuses = (data) => {
   let abilityArray = [];
@@ -37,23 +26,24 @@ const getIncreaseAmount = (data) => {
 }
 
 
-// export const getAvgAge = (ageDescription) => {
+
+export const getAvgAge = (ageDescription) => {
   
-//   // let regExp = /([Aa]dult)/g;
-//   let numRegExp = /(\d+)/g;
-//   let array = [];
-//   let ageDescriptionArray = array.concat(ageDescription.match(numRegExp));
-//   if(ageDescriptionArray.length === 0){
-//     return 30;
-//   }else if(ageDescriptionArray.length >= 3){
-//     return ageDescriptionArray[2];
-//   }else if (ageDescriptionArray.length < 3){
-//     return ageDescriptionArray[0];
-//   }else{
-//     return ageDescriptionArray[1];
-//   }
+  let regExp = /([Aa]dult)/g;
+  let numRegExp = /(\d+)/g;
+  let array = [];
+  let ageDescriptionArray = array.concat(ageDescription.match(numRegExp));
+  if(ageDescriptionArray.length === 0){
+    return 30;
+  }else if(ageDescriptionArray.length >= 3){
+    return ageDescriptionArray[2];
+  }else if (ageDescriptionArray.length < 3){
+    return ageDescriptionArray[0];
+  }else{
+    return ageDescriptionArray[1];
+  }
   
-// }
+}
 
 const combineAbilityBonuses = (abilityName, bonusAmount) => {
   let bonusObject = {};
@@ -68,89 +58,68 @@ const combineAbilityBonuses = (abilityName, bonusAmount) => {
 
 
 
-export const AddRaceDetails = (props) => {
-  
-  const [pageData, setPageData] = useState(null);
-  const [, updateState] = useState();
-  const forceUpdate = React.useCallback(() => updateState({}), []);
+export const AddRaceDetails = ({formField, data }) => {
   const {
-    formField: {
-      speed,
-      size,
-      age,
-      
-    }
-  } = props;
-  const { currentCharacter, setCurrentCharacter } = useContext(FormContext);
+    form_speed,
+    form_size,
+    form_age,
+    form_abilityBonuses
+  } = formField;
+  console.log(data)
+  const { 
+    ability_bonuses,
+    age,
+    alignment,
+    index,
+    language_desc,
+    languages,
+    name,
+    size,
+    size_description,
+    speed,
+    starting_proficiencies,
+    subraces,
+    traits,
+    url
+    } = data || {};
+  // console.log(data, 'stuff');
   
+  const [isLoading, setIsLoading] = useState(true);
+  const {setCurrentCharacter} = useContext(FormContext);
   
-  
-const userAction = async () => {
-  fetch('https://www.dnd5eapi.co/api/races').then(response => {
-    console.log(response);
-    if(!response.ok) {
-      throw new Error(`${response.status}: ${response.statusText}`);
-    } else {
-      return response.json();
-    }
-  }).then((jsonifiedResponse) => {
-    setPageData(jsonifiedResponse.results);
-    console.log(jsonifiedResponse.results);
-    
-  })
-}
-  // console.log(currentCharacter);
-  useEffect(() => {
-    setPageData(getDetails(currentCharacter));
-    
-    console.log(currentCharacter)
-  }, [currentCharacter])
-  
-  const getDetails = (currentCharacter) => {
-    let characterBonusAmounts = null;
-    let characterAbilityBonuses = null;
-    if(typeof currentCharacter !== undefined){
-      const data = userAction();
-      console.log(data);
-      const raceDetails = getRaceInfo(data, currentCharacter.race)[0];
-      if(raceDetails.ability_bonuses !== undefined){
-         characterAbilityBonuses = getAbilityBonuses(raceDetails.ability_bonuses);
-         characterBonusAmounts = getIncreaseAmount(raceDetails.ability_bonuses);
-      }
-      // console.log(pageData);
-      return [raceDetails, characterAbilityBonuses, characterBonusAmounts];
-      
-    }
-  }
-  let raceDetails = null;
   let characterAbilityBonuses = null;
   let characterBonusAmounts = null;
+  let abilityBonuses = null;
   
-  if(pageData !== null){
-     raceDetails = pageData[0];
-     characterAbilityBonuses = pageData[1];
-     characterBonusAmounts = pageData[2];
-     const abilityBonuses = combineAbilityBonuses(characterAbilityBonuses, characterBonusAmounts);
-    setCurrentCharacter(prevState => ({
-      ...prevState,
-      abilityBonuses
-    }));
-  }
+  // if(ability_bonuses !== undefined){
+    
+  //   characterAbilityBonuses = getAbilityBonuses(ability_bonuses);
+  //   characterBonusAmounts = getIncreaseAmount(ability_bonuses);
+  //   abilityBonuses = combineAbilityBonuses(characterAbilityBonuses, characterBonusAmounts);
+  //   setCurrentCharacter(prevState => ({
+  //     ...prevState,
+  //     abilityBonuses
+  //   }));
+  //   if(abilityBonuses !== null){
+      
+  //     setIsLoading(false)
+  //   }
+  // }
  
   // const averageRaceAge = getAvgAge(raceDetails.age);
   
-  return (
+  return data? (
     <Box>
       <Box>
 
-        <InputField name={speed.name} label={speed.label} fullWidth/>
-        <InputField name={size.name} label={size.label} fullWidth/> 
-        <InputField name={age.name} label={age.label} fullWidth/>
+        <InputField name={form_speed.name} label={form_speed.label} fullWidth/>
+        <InputField name={form_size.name} label={form_size.label} fullWidth/> 
+        <InputField name={form_age.name} label={form_age.label} fullWidth/> 
         {/* <InputField name={abilityBonuses.name} label={abilityBonuses.label} fullWidth/> */}
       </Box>
-      {pageData !== null ?  
-     <Box display="grid" justifyContent="start" m='20px'>
-        <Typography>Common Stats: {raceDetails.name}</Typography>
+      {/* {isLoading === false ?   */}
+      <Box display="grid" justifyContent="start" m='20px'>
+        <Typography>Common Stats: {name}</Typography>
         <Box p='8px'>
           <Box sx={{
             display: 'grid',
@@ -163,12 +132,12 @@ const userAction = async () => {
               margin: '8px'
             }}> 
               <Box>
-                <Typography m='3px'>Speed: {raceDetails.speed} feet</Typography>
-                <Typography m='3px'>Size: {raceDetails.size}</Typography> 
+                <Typography m='3px'>Speed: {speed} feet</Typography>
+                <Typography m='3px'>Size: {size}</Typography> 
               </Box>
               <Box>
                 <Typography>Languages:</Typography>
-                {raceDetails.languages.map((lang, index) => (
+                {languages.map((lang, index) => (
                   <Typography key={index}>
                     {lang.name}
                   </Typography>
@@ -176,51 +145,51 @@ const userAction = async () => {
               </Box>
               <Box>
                 <Typography>Traits: </Typography>
-                {raceDetails.traits.map((trait, index) => (
+                {traits.map((trait, index) => (
                   <Typography key={index}>
                     {trait.name}
                   </Typography>
                 ))}
               </Box>
-              <Box sx={{display: 'grid', justifyContent: 'center'}}>
+              {/* <Box sx={{display: 'grid', justifyContent: 'center'}}>
                 <Typography>Ability Bonuses</Typography>
                 <Box sx={{display: 'grid', gridTemplateColumns: '.3fr .3fr'}}>
-                  <Box>{characterAbilityBonuses.map((ability, index) => (
+                  <Box>{characterAbilityBonuses?.map((ability, index) => (
                     <Typography key={index}>{ability}</Typography>
                   ))}</Box>
-                  <Box>{characterBonusAmounts.map((bonus, index) => (
+                  <Box>{characterBonusAmounts?.map((bonus, index) => (
                     <Typography key={index}> + {bonus}</Typography>
                   ))}</Box>
                 </Box>
-              </Box>
+              </Box> */}
             </Box>
             <Box sx={{
               display: 'grid',
             }}>
               
-              <Typography>Age: {raceDetails.age}</Typography>
+              <Typography>Age: {age}</Typography>
             </Box>
             <Box sx={{
               display: 'grid',
             }}>
-              <Typography>{raceDetails.size_description}</Typography>
+              <Typography>{size_description}</Typography>
             </Box>
             <Box sx={{
               display: 'grid',
             }}>
               
-              <Typography>{raceDetails.language_desc}</Typography>
+              <Typography>{language_desc}</Typography>
             </Box>
           </Box>
         </Box>
       </Box>
-     : null }  
+     {/* : <Loading/> }   */}
       <Box>
         <Typography></Typography>
       </Box> 
      
-    </Box>
-  );
+     </Box>
+  ): null;
 };
 
 
