a4a24f3767d67a10034c91fe2102d03590b156b5
Liam Cammpbell <liam22campbell@gmail.com>
2023-04-21 21:35:18 -0700

    View an display an item

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,9 @@
 import Loading from './Components/loading';
 import {Box} from '@mui/material';
+
+import { ItemList } from './Components/Skills/SkillList'
 const Home = () => {
+  
   return (
     <Box sx={{
       padding: '10px',
@@ -8,7 +11,12 @@ const Home = () => {
       display: 'flex',
       justifyContent: 'center',
     }}>
+
+      <Box>
       <Loading/>
+      <ItemList/>
+      
+      </Box>
     </Box>
   );
 };
