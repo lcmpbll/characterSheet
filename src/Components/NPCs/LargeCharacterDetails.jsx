5257e1a61eb4c28d50006ddbced770d5d3070c51
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-16 14:40:26 -0700

    Add speed, initiative, and ac

--- src/Components/NPCs/LargeCharacterDetails.jsx
+++ src/Components/NPCs/LargeCharacterDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Loading from '../loading';
-import { Box } from '@mui/material'
+import { Box } from '@mui/material';
+import { ItemList } from '../Skills/SkillList';
 
 const LargeCharacterDetails = ({npc}) => {
   console.log(npc,'ln 6')
@@ -8,15 +9,44 @@ const LargeCharacterDetails = ({npc}) => {
     return <Loading/>
   }
   return (
-    <Box sx={{display: 'flex', flexDirection: 'row', flex: 11}}>
+    <Box sx={{display: 'flex', flexDirection: 'column', flex: 11}}>
       <Box sx={{flex: 11}}>
         <CharacterHeader name={npc.name} type={npc.type} sub_type={npc.sub_type}/>
+      </Box>
+      <Box sx={{display: 'flex', flexDirection: 'column', flex: 11}}>
+        <Box sx={{display: 'flex', flexDirection: 'column', flex: 3}}>
+          <ItemList skillsList={npc.ability_score} />
+        </Box>
+        <Box sx={{display: 'flex', flex: 2}}>
+          <SmallStatsBoxes speed={npc.speed} ac={npc.ac} initiative={npc.ability_score[2].plus}/>
+        </Box>
       </Box>
     </Box>
   )
 }
 
 export default LargeCharacterDetails
+
+
+const SmallStatsBoxes = (props) => {
+  const {speed, ac, initiative} = props;
+  return (
+    <Box sx={{display: 'flex', border: '3px double grey'}}>
+      <Box sx={{display: 'flex', flexDirection: 'column', height: '100px', width: '100px', border: '1px solid black'}}>
+        <h4>{ac}</h4>
+        <p>Armor Class</p>
+      </Box>
+      <Box sx={{display: 'flex', flexDirection: 'column', height: '100px', width: '100px', border: '1px solid black'}}>
+        <h4>{initiative > 0 ? '' : '-'}{initiative}</h4>
+        <p>Initiative</p>
+      </Box>
+      <Box sx={{display: 'flex', flexDirection: 'column', height: '100px', width: '100px', border: '1px solid black'}}>
+        <h4>{speed}</h4>
+        <p>Speed</p>
+      </Box>
+    </Box>
+  )
+}
 
 const CharacterHeader = (props) => {
   const {name, type, sub_type} = props;
