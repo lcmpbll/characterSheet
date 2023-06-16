11e15a00327750c3ed9e82404e79f31a7274e897
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-16 12:40:20 -0700

    Adjust height and width.

--- src/Components/Skills/SkillBadge.jsx
+++ src/Components/Skills/SkillBadge.jsx
@@ -3,14 +3,14 @@ import { Box, Typography } from '@mui/material'
 
 export const SkillBadge = ({skill}) => {
   return (
-    <Box sx={{ border: '5px solid grey', borderRadius: '8%'}}>
+    <Box sx={{ border: '5px solid grey', width: '100px', height: '100px', borderRadius: '8%'}}>
       <Box>
         <Typography>{skill?.name}</Typography>
       </Box>
       <Box>
         <Typography>{skill?.amount}</Typography>
       </Box>
-      <Box sx={{ border: '2px solid grey', width: '50%', borderRadius: '40%', margin: '0 auto' }}><Typography>{skill.plus >= 0 ? '+' : null} {skill?.plus}</Typography></Box>
+      <Box sx={{ border: '2px solid grey', width: '30px', height: '30px',  borderRadius: '40%', margin: '0 auto' }}><Typography>{skill.plus >= 0 ? '+' : null} {skill?.plus}</Typography></Box>
     </Box>
   );
 }
