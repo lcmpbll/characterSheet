7e67972b02ced49b2f6047bad987c06a41ba7500
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-16 12:35:50 -0700

    Add NPC link

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -3,11 +3,15 @@ import { Link } from "react-router-dom";
 import { Box } from "@mui/material";
 import { AiFillHome, AiOutlineHome } from "react-icons/ai";
 import { MdPersonAddAlt, MdPersonAddAlt1 } from "react-icons/md";
+import { FaAddressBook, FaRegAddressBook } from 'react-icons/fa';
 
 const Menu = () => {
   const [selected, setSelected] = useState("home");
   return (
-    <Box display="flex" flexDirection="row" justifyContent="space-evenly">
+    <Box display="flex" flexDirection="row" justifyContent="space-evenly" width="200px" height='20px' >
+      <Box onClick={() => {setSelected("npc")}}>
+        {selected === 'npc' ? <FaAddressBook/> : <Link to={"npc"}> <FaRegAddressBook/> </Link>}
+      </Box>
       <Box
         onClick={() => {
           setSelected("home");
