91c748d526def41b82f5d0530e8f086159a73ecb
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-16 16:14:21 -0700

    Add NPC List

--- src/Components/NPCs/index.jsx
+++ src/Components/NPCs/index.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NPCDetails from './NPCdetails';
 import creatureList from '../../data/creatures.json';
+import LargeCharacterDetails from './LargeCharacterDetails';
 
 import { Box } from '@mui/material';
 
  const RegularList = ({
   items, 
   resourceName, 
-  itemComponent: ItemComponent
+  itemComponent: ItemComponent,
+
+  
 }) => {
 
   return(
@@ -21,16 +24,22 @@ import { Box } from '@mui/material';
 }
 
 const NPC = () => {
- 
+ const [selectedCharacter, setSelectedCharacter] = useState(creatureList[0]);
+ console.log(selectedCharacter)
+
   return (
     <>
-      <div>NPC</div>
-    
-      <Box>
-        <RegularList items={creatureList} resourceName='NPC' itemComponent={NPCDetails}/>
-        
+      <Box sx={{widht: '100%', height: '100%', display: 'flex', justifyContent: 'space-evenly', flex: 6, padding: '10px'}}>
+        <Box sx={{flex: 2}}>
+          <RegularList items={creatureList} resourceName='NPC' itemComponent={NPCDetails} />
+        </Box>
+        <Box sx={{flex: 4}}>
+          {selectedCharacter === null ? 
+            <p>Select Character</p> :
+            <LargeCharacterDetails npc={selectedCharacter}/> 
+          }
+        </Box>
       </Box>
-  
     </>
   )
 }
