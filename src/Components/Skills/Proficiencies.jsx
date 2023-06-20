ef025415cd655076597cba92b3091662e5447609
Liam Cammpbell <liam22campbell@gmail.com>
2023-06-20 10:51:29 -0700

    Add a proficiencies List

---
+++ src/Components/Skills/Proficiencies.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+
+export const ProficienciesList = ({profs = {
+  Arc: 3,
+  His: 3,
+  Inv: 1,
+  Nat: 4,
+}}) => {
+  const profieciencies = [
+    { 
+      name: 'Acrobatics',
+      skill: 'DEX',
+      ref: 'Acr'
+    },   
+    { 
+      name: 'Animal Handling',
+      skill: 'WIS',
+      ref: 'Ani'
+    },
+    { 
+      name: 'Arcana',
+      skill: 'INT',
+      ref: 'Arc'
+    },
+    { 
+      name: 'Athletics',
+      skill: 'STR',
+      ref: 'Ath'
+    },
+    { 
+      name: 'Deception',
+      skill: 'CHA',
+      ref: 'Dec'
+    },
+    { 
+      name: 'History',
+      skill: 'INT',
+      ref: 'His'
+    },
+    { 
+      name: 'Insight',
+      skill: 'WIS',
+      ref: 'Ins'
+    },
+    { 
+      name: 'Intimidation',
+      skill: 'CHA',
+      ref: 'Int'
+    },
+    { 
+      name: 'Investigation',
+      skill: 'INT',
+      ref: 'Inv'
+    },
+    { 
+      name: 'Medicine',
+      skill: 'WIS',
+      ref: 'Med'
+    },
+    { 
+      name: 'Nature',
+      skill: 'INT',
+      ref: 'Nat'
+    },
+    { 
+      name: 'Perception',
+      skill: 'WIS',
+      ref: 'Perc'
+    },
+    { 
+      name: 'Performance',
+      skill: 'CHA',
+      ref: 'Perf'
+    },
+    { 
+      name: 'Persuasion',
+      skill: 'CHA',
+      ref: 'Pers'
+    },
+    { 
+      name: 'Religion',
+      skill: 'INT',
+      ref: 'Rel'
+    },
+    { 
+      name: 'Sleight of Hand',
+      skill: 'DEX',
+      ref: 'Soh'
+    },
+    { 
+      name: 'Stealth',
+      skill: 'DEX',
+      ref: 'Ste'
+    },
+    { 
+      name: 'Survival',
+      skill: 'WIS',
+      ref: 'Sur'
+    },
+  ]
+  
+  return (
+    <>
+      {profieciencies.map((prof, index) => {
+        return(
+          <div style={{display: 'flex', flexDirection: 'row',}} key={index}>
+            <p style={{marginRight: '3px', borderBottom: '2px solid black', size: '20px', width: '2rem'}}>{profs[prof.ref] ? profs[prof.ref] : null}</p>
+            <p>{prof.name} ({prof.skill})</p>
+          </div>
+        )
+      })}
+    </>
+  )
+}
