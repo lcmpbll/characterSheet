5c92a7274f92a801179460db87ff14f32c6da282
Liam Cammpbell <liam22campbell@gmail.com>
2023-07-09 16:20:17 -0700

    Add item details display

--- src/Components/Items/LargeDetailItem.jsx
+++ src/Components/Items/LargeDetailItem.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import Loading from '../loading';
+import { DetailsHeader, ActionsBox, OtherAbilities } from '../NPCs/LargeCharacterDetails'
 
-const LargeDetailItem = () => {
+const LargeDetailItem = ({item}) => {
+  if(!item) return <Loading/>
+  console.log(item);
   return (
-    <Box>
+    <Box sx={{display: 'flex', flexDirection: 'column'}}>
+      <Box>
+        <DetailsHeader name={item.name} type={item.type} sub_type={item.sub_type} desc={item.description} resource={item.type}/>
+      </Box>
+      <Box>
+        <ActionsBox actions={item.abilities}/>
+      </Box>
+      <Box>
+        {item.abilities?.special_ability ? 
+          <OtherAbilities ability={item.abilities?.special_ability} /> :
+        null }
+      </Box>
     </Box>
   )
 }
