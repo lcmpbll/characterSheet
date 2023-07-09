713613bd9a7689af54b369e48953e76ef95e94b2
Liam Cammpbell <liam22campbell@gmail.com>
2023-07-09 15:44:57 -0700

    Move NPC to smallitemdetail

--- src/Components/Lists/SmallDetailItem.jsx
+++ src/Components/Lists/SmallDetailItem.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Loading from '../loading';
 import { Box } from '@mui/material';
 
-const SmallDetailNPC = ({NPC}) => {
-  if(!NPC){
+const SmallDetailItem = ({Item}) => {
+  if(!Item){
     return <Loading/>
   }
-  const {name, sub_type} = NPC;
+  const {name, sub_type} = Item;
 
   return (
     <Box sx={{display: 'flex', alignItems: 'flex-start'}}>
@@ -15,4 +15,4 @@ const SmallDetailNPC = ({NPC}) => {
   )
 }
 
-export default SmallDetailNPC;
+export default SmallDetailItem;
