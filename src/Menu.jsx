6bd755022dae904339f1922d862223e1215d9d3b
Liam Cammpbell <liam22campbell@gmail.com>
2023-07-09 15:45:21 -0700

    Add links to items page

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -4,6 +4,8 @@ import { Box } from "@mui/material";
 import { AiFillHome, AiOutlineHome } from "react-icons/ai";
 import { MdPersonAddAlt, MdPersonAddAlt1 } from "react-icons/md";
 import { FaAddressBook, FaRegAddressBook } from 'react-icons/fa';
+import { GiSwapBag } from 'react-icons/gi';
+import { CiBag1 } from 'react-icons/ci';
 
 const Menu = () => {
   const [selected, setSelected] = useState("home");
@@ -11,6 +13,16 @@ const Menu = () => {
     <Box display="flex" flexDirection="row" justifyContent="space-evenly" width="200px" height='20px' >
       <Box onClick={() => {setSelected("npc")}}>
         {selected === 'npc' ? <FaAddressBook/> : <Link to={"npc"}> <FaRegAddressBook/> </Link>}
+      </Box>
+      <Box onClikc ={() => {setSelected("items")}}>
+        {selected === "items"? (
+          <GiSwapBag/>
+        ) :
+        (
+          <Link to={'items'}>
+            <CiBag1/>
+          </Link>
+        )}
       </Box>
       <Box
         onClick={() => {
