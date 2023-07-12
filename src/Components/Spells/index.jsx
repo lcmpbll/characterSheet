755a911a4e0f14547e051b7deae2219a2c656ffc
Liam Cammpbell <liam22campbell@gmail.com>
2023-07-11 21:45:52 -0700

    Add spell details

--- src/Components/Spells/index.jsx
+++ src/Components/Spells/index.jsx
@@ -4,6 +4,10 @@ import { SmallClickableDetailItem } from '../Lists/SmallDetailItem';
 import { withData } from '../../HOC/withFormData';
 import { SplitScreen } from '../Divisions/SplitScreen';
 import { ApiList } from '../Lists';
+import { FaHeadSideVirus } from 'react-icons/fa';
+import { GiJuggler } from 'react-icons/gi';
+import { MdOutlineCheckCircleOutline } from 'react-icons/md';
+import { RiNotificationOffLine } from 'react-icons/ri';
 
 export const SpellContext = createContext();
 const Spells = () => {
@@ -26,7 +30,7 @@ const Spells = () => {
     
     <SplitScreen
     leftWeight={1}
-    rightWeight={5}
+    rightWeight={4}
     >
       <MemorizedSpellList />
       <SpellDetailsWithData/>
@@ -50,6 +54,7 @@ const SpellDetails = ({data}) => {
     ritual,
     range,
     school,
+    classes,
     subclasses,
     url,
     desc
@@ -57,9 +62,32 @@ const SpellDetails = ({data}) => {
   console.log(data);
   
   return data ? (
-    <Box sx={{height: '100%', width: '100%', display: 'flex', justifyContent: 'flex-start', flexDirection: 'column'}}>
+    <Box sx={{height: '100%', width: '100%', display: 'flex', justifyContent: 'flex-start', flexDirection: 'column', margin: '1rem'}}>
       <Box sx={{display: 'flex', justifyContent: 'flex-start', alignItems: 'baseline'}}>
-        <h1>{name}</h1>
+        <Box sx={{display: 'flex', alignItems: 'flex-start', justifyContent: 'baseline', flexDirection: 'column'}}>
+          <h1>{name}</h1>
+          <p></p>
+        </Box>
+        <Box sx={{display: 'flex', alignItems: 'flex-start', justifyContent: 'baseline', flexDirection: 'column'}}>
+          {classes.map((spellClass, index) => {
+            return (
+              <p key={index}>{spellClass.name}</p>
+            )
+          })}
+        </Box>
+        <Box sx={{display: 'flex', width: '100%', alignItems: 'flex-end', flexDirection: 'column'}}>
+          <p>School: {school.name}</p>
+          <p>Level: {level}</p>
+        </Box>
+      </Box>
+      <Box sx={{}}>
+        <Box sx={{display: 'flex', justifyContent: 'space-between', border: '2px solid black', padding: '1rem', width: '100%'}}>
+          <p>Range: {range}</p>
+          <p>Casting Time: {casting_time}</p>
+          <p>Duration: {duration}</p>
+          <p>Concentration: {concentration ? <FaHeadSideVirus/> : <GiJuggler/> }</p>
+          <p>Ritual: {ritual ? <MdOutlineCheckCircleOutline/> : <RiNotificationOffLine/> }</p>
+        </Box>
       </Box>
       <Box>
         {desc.map((description, index) => {
@@ -70,13 +98,18 @@ const SpellDetails = ({data}) => {
           )
         })}
       </Box>
-      <Box sx={{display: 'flex', justifyContent: 'space-between', border: '2px solid black', padding: '3px'}}>
-        <p>Range: {range}</p>
-        <p>Casting Time: {casting_time}</p>
-        <p>Duration: {duration}</p>
-        <p>Concentration: {concentration ? "yes" : "nah"}</p>
-        <p>Ritual: {ritual ? "yes" : "nah"}</p>
-      </Box>
+      {higher_level.length > 0 ? 
+      <Box>
+        <h5>At Higher Level</h5>
+          {higher_level?.map((description, index) => {
+            return (
+              <Box>
+                <p>{description}</p>
+                </Box>
+            )
+          })}
+      
+      </Box> : null }
     </Box>
   ) : <h1> Select a Spell</h1>;
   
