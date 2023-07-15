daf71912e61da6fd39d459bcce8f39affe0906e1
Liam Cammpbell <liam22campbell@gmail.com>
2023-07-15 13:05:02 -0700

    Add clickable function to regular list

--- src/Components/Lists/index.jsx
+++ src/Components/Lists/index.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import Loading from '../loading';
 import { Box } from '@mui/material';
+import InputField from '../../FormFields/InputField';
 
 
 export const RegularList = ({
   items, 
   resourceName, 
-  itemComponent: ItemComponent
+  itemComponent: ItemComponent,
+  handleItemClick
 }) => {
   return(
     <>
       {items.map((item, index) => (
-        <ItemComponent key={index} {...{[resourceName]: item}} />
+        <ItemComponent key={index} {...{[resourceName]: item}}  handleItemClick={handleItemClick}/>
       ))}
     </>
   );
+}
+
+export const Sorter = ({list}) => {
+  return (
+    <Box>
+      <Box>
+        {/* <InputField name={'search'} label={'search'} fullWidth/> */}
+        
+      </Box>
+    </Box>
+  )
 }
 
 export const ApiList = ({
@@ -31,6 +44,7 @@ export const ApiList = ({
 
   return data ? (
     <Box sx={{height: '1750px', overflowY: 'auto'}}>
+      <Sorter list={results}/>
       <Box sx={{height: '100%'}}>
         {results.map((item, index) => (
           <ItemComponent key={index} {...{[resourceName]: item}} handleItemClick={handleItemClick} />
